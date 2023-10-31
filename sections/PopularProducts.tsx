eff4e16093198519b23c6a160fa064b2d7087e14
khurrams4 <khurram.shahzad@itcurves.com>
2023-11-01 03:11:52 +0500

    otherSources...

--- sections/PopularProducts.tsx
+++ sections/PopularProducts.tsx
@@ -8,10 +8,11 @@ const PopularProducts = () => {
         <h2 className='text-4xl font-palanquin font-bold'>
           Our <span className='text-coral-red'> Popular </span> Products
         </h2>
-        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
+        <br/>
+        <div className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top-notch quality and style with our sought-after
           selections. Discover a world of comfort, design, and value
-        </p>
+        </div>
       </div>
 
       <div className="grid grid-cols-4 gap-4 ">
