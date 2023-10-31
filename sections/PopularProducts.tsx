372853c7348a8088d19551cffaf0f410ac3a5e31
khurrams4 <khurram.shahzad@itcurves.com>
2023-11-01 03:27:53 +0500

    mb...

--- sections/PopularProducts.tsx
+++ sections/PopularProducts.tsx
@@ -4,15 +4,15 @@ import { PopularProductCard } from "../components";
 const PopularProducts = () => {
   return (
     <section id='products' className='w-full max-sm:mt-12 '>
-      <div className='flex flex-row justify-start gap-5'>
+      <div className='flex flex-col justify-start gap-5 mb-12'>
         <h2 className='text-4xl font-palanquin font-bold'>
           Our <span className='text-coral-red'> Popular </span> Products
         </h2>
-        <br/>
-        <div className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+        
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top-notch quality and style with our sought-after
           selections. Discover a world of comfort, design, and value
-        </div>
+        </p>
       </div>
 
       <div className="grid grid-cols-4 gap-4 ">
